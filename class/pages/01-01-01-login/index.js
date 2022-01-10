215beb6c42cc053b1f063284110700fdf418dad9
Kimkeebeom <kkb9102@naver.com>
2022-01-10 16:27:03 +0900

    codecamp-frontend-05-github

---
+++ class/pages/01-01-01-login/index.js
@@ -0,0 +1,14 @@
+import {MyTitle, MyEmail} from '../../styles/emotion'
+
+
+export default function LoginPage(){
+
+    return (
+        <div>
+            <MyTitle>안녕하세요!! 이모션 페이지입니다.</MyTitle>
+            <input type="text"/>
+            <img src="/images/lotto.png" />
+        </div>
+    )
+
+}
