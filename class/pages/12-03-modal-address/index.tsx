5f93068e449703c12ccc8400a757c3689cdaedb1
Kimkeebeom <kkb9102@naver.com>
2022-01-25 15:57:19 +0900

    날라간 12일차 복구

---
+++ class/pages/12-03-modal-address/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+import DaumPostcode from "react-daum-postcode";
+
+export default function ModalAddressPage() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [zonecode, setZonecode] = useState("");
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+const onCompleteDaumPostCode = (data: any) => {
+  // console.log(data);
+  setAddress(data.address);
+  setZonecode(data.zonecode);
+  setIsModalVisible(false);
+}
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        우편번호 검색
+      </Button>
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <DaumPostcode onComplete={onCompleteDaumPostCode}/>
+      </Modal>
+    </>
+  );
+}
