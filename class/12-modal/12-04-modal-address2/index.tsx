f55c176306a237312ddaf1c04be820e785fd592a
Kimkeebeom <kkb9102@naver.com>
2022-01-25 15:39:13 +0900

    test

---
+++ class/12-modal/12-04-modal-address2/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+import DaumPostcode from "react-daum-postcode";
+
+export default function ModalAddressPage() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [, setPassword] = useState("");
+  const [address, setAddress] = useState("");
+  const [zonecode, setZonecode] = useState("");
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onCompleteDaumPostCode = (data: any) => {
+    // console.log(data);
+    setAddress(data.address);
+    setZonecode(data.zonecode);
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        우편번호 검색
+      </Button>
+      {isModalVisible && (
+        <Modal
+          title="Basic Modal"
+          visible={true}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <DaumPostcode onComplete={onCompleteDaumPostCode} />
+        </Modal>
+      )}
+    </>
+  );
+}
