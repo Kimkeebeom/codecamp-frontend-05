4358e12f7cf4bc6cdf383f39b7a9304c9a8598ac
Kimkeebeom <kkb9102@naver.com>
2022-02-07 14:19:41 +0900

    디비버 설정

--- class_backend/Board.postgres.ts
+++ class_backend/Board.postgres.ts
@@ -1,7 +1,7 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { Column, Entity, PrimaryGeneratedColumn, BaseEntity } from 'typeorm'
 
 @Entity()
-export class Board {
+export class Board extends BaseEntity {
     
     @PrimaryGeneratedColumn('increment') // 특정 게시물을 만들거나 상품을 만들 때 특정 primary id값이 필요할 떄!
     number!: number // 앞은 컬럼명 뒤에는 타입, 뒤에 느낌표를 붙여주게 되면 얘는 반드시 있을거야라는 의미!
@@ -14,5 +14,9 @@ export class Board {
 
     @Column({ type: "text" })
     age!: number
+
+    // default: null 
+    @Column({ type: "timestamp", default: null, nullable: true })
+    deletedAt!: Date
 
 }
---
+++ class_backend/bbb.ts
@@ -0,0 +1,12 @@
+// decorator 예제
+
+function qqq(aaa: any){
+
+    console.log('=====================')
+    console.log(aaa)
+    console.log('=====================')
+
+}
+
+@qqq
+class MyBoard {}
