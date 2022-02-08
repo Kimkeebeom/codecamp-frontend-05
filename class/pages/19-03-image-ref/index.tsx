06921ea9295ccb04419f060f33fda1c9cb362166
Kimkeebeom <kkb9102@naver.com>
2022-02-08 23:52:58 +0900

    19일차 복습

--- class/pages/19-03-image-ref/index.tsx
+++ class/pages/19-03-image-ref/index.tsx
@@ -28,13 +28,13 @@ export default function ImageValidationPage(){
         if(!isValid) return
 
         try {
-            const result = await uploadFile({
+            const result = await uploadFile({ 
                 // variables : 어떤 파일을 업로드 할지?
                 variables: {
                     file: file // key와 value가 같으므로 file, 숏앤드프로퍼티로 작성 가능
                 }
             })
-            console.log("result",result.data?.uploadFile.url)
+            console.log("result",result.data?.uploadFile.url) // 파일을 스토리지에서 url로 받아오고 그 url이 result에 저장된다!!!!
     
             setImage(result.data?.uploadFile.url || "") // 이미지가 스트링타입으로 추론이 되었는데 없을 수도 있기때문에 빈문자열로 보여줘! 
         } catch (error: any) {
@@ -43,7 +43,7 @@ export default function ImageValidationPage(){
     }
 
     const onClickImage = () => {
-        fileRef.current?.click();
+        fileRef.current?.click(); // 이미지를 클릭했지만 fileRef가 클릭되게끔 해야하기 때문에 fileRef를 쓴다.
     }
 
     // const myStyle = {
