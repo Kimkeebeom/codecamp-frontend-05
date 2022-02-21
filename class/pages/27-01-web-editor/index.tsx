43bf615a89561e1a8981812016ebf33672dbd9d0
Kimkeebeom <kkb9102@naver.com>
2022-02-21 10:27:58 +0900

    react-quill 설치

---
+++ class/pages/27-01-web-editor/index.tsx
@@ -0,0 +1,23 @@
+import ReactQuill from 'react-quill';
+import "react-quill/dist/quill.snow.css";
+
+export default function WebEditorPage() {
+
+    const hanndleChange = (value: string) => {
+        console.log(value)
+    }
+
+    return (
+      <div>
+        작성자 : <input type="text" />
+        <br />
+        비밀번호 : <input type="password" />
+        <br />
+        제목 : <input type="text" />
+        <br />
+        내용 : <ReactQuill onChange={}/>
+        <br />
+        <button>등록하기</button>
+      </div>
+    );
+  }
