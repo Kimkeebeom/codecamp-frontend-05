449809f88545375ca3d681390eb477c19500fdf9
Kimkeebeom <kkb9102@naver.com>
2022-02-22 09:44:15 +0900

    상품등록 기능구현 2일차

---
+++ freeboard-frontend/pages/product/new/index.tsx
@@ -0,0 +1,22 @@
+// import styled from "@emotion/styled"
+
+// const Wrapper = styled.div`
+//     box-sizing: border-box;
+//     font-size: 100px;
+//     color: white;
+// `
+
+export default function productWrite(){
+
+
+    return(
+        // <Wrapper>
+        //     상품등록 페이지🛍
+        // </Wrapper>
+        <div>
+            <div>
+                이미지 선택
+            </div>
+        </div>
+    )
+}
