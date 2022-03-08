d9e81fc0b7fe6d4f987c83b80ff5e8866127aadb
Kimkeebeom <kkb9102@naver.com>
2022-03-08 17:45:35 +0900

    정적배포

--- class_build/next.config.js
+++ class_build/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
+  trailingSlash: true, // Slash를 기준으로 폴더안에 파일을 만들어줘!
   swcMinify: false
 }
 
