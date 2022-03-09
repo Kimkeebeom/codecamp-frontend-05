8b3528d4d8ec6c98b2e1b5cd30df66fc8c02a7d5
Kimkeebeom <kkb9102@naver.com>
2022-03-09 23:47:10 +0900

    Update opengraph

--- class_build/pages/boards/[boardId]/index.js
+++ class_build/pages/boards/[boardId]/index.js
@@ -1,12 +1,21 @@
 import {useRouter} from 'next/router'
+import Head from 'next/head'
 
 export default function BoardsDetailPage(){
     const router = useRouter()
 
     return (
         <div>
-            안녕하세요! 게시글 상세페이지입니다, 
-            게시글 ID는 {router.query.boardId}입니다!!
+            <Head>
+                <meta property="og:title" content="복습" />
+                <meta property="og:description" content="복습만이 살길입니다~" />
+                <meta property="og:image" content="https://www.100ssd.co.kr/news/photo/201911/65167_45054_4842.jpg"/>
+            </Head>
+            <div>
+                안녕하세요! 게시글 상세페이지입니다, 
+                게시글 ID는 {router.query.boardId}입니다!!
+            </div>
         </div>
+        
     )
 }
